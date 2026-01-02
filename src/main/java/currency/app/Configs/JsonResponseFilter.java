e79f31dbf18b817089ae764a6af80fa6390dbd59
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2026-01-02 16:59:07 +0500

    общий фильтр для POST запросов для установки заголовков

---
+++ src/main/java/currency/app/Configs/JsonResponseFilter.java
@@ -0,0 +1,32 @@
+package currency.app.Configs;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class JsonResponseFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) {}
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        httpResponse.setContentType("application/json");
+        httpResponse.setCharacterEncoding("UTF-8");
+
+        httpResponse.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        httpResponse.setHeader("Pragma", "no-cache");
+        httpResponse.setHeader("Expires", "0");
+
+        chain.doFilter(request,response);
+    }
+
+    @Override
+    public void destroy() {}
+}
